04f952a0139197e2c46b8a48f8d5ae6e07762e91
Loo-ke <looke2930@gmail.com>
2024-04-08 20:59:36 +0900

    2024-04-08 : 12 수강

--- JS/100days/12/app.js
+++ JS/100days/12/app.js
@@ -1,1 +1,30 @@
-document.body.children[1].children[0].href='https://www.google.com'
+// document.body.children[2].children[0].href='https://www.google.com'
+  // let anchor = document.getElementById('anchor')
+
+  // anchor.href='hhtps://google.com'
+  // let anchor2 = document.querySelector('#anchor')
+
+  // let h1 = document.body.firstElementChild
+  // console.log(h1)
+
+  // let parent = h1.parentElement
+  // console.log(parent)
+
+  // let children2 = parent.children[1]
+
+  // console.log(children2)
+
+  // let h1Id = document.getElementById('h1')
+  // console.log(h1Id)
+
+  // let pId = document.querySelector("#p")
+  // console.log(pId)
+  // pId.textContent = 'hihhihihi'
+
+let anchor = document.createElement('a')
+anchor.href = 'https://google.com'
+anchor.textContent = 'to GOOGLE'
+
+let first = document.querySelector('p')
+
+first.appendChild(anchor)
